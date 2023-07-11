da092225343b042949e6e2cdec667f02450a3517
vlad <vlad@gmail.com>
2023-07-12 00:36:14 +0800

    hi, fixed small issues

--- src/App.js
+++ src/App.js
@@ -33,7 +33,7 @@ function Board({ xIsNext, squares, onPlay }) {
   }
 
   return (
-    <div>
+    <React.Fragment>
       <div className="status">{status}</div>
       <div className="board-row">
         <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
@@ -50,7 +50,7 @@ function Board({ xIsNext, squares, onPlay }) {
         <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
         <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
       </div>
-    </div>
+    </React.Fragment>
   );
 }
 
